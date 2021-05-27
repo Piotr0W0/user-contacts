8191a1841c2399a0a5bad9f42f8509cd7e744db2
Piotr <piotr.witczak.01@gmail.com>
2021-05-27 16:59:33 +0200

    Heroku.

--- src/main/java/pl/edu/wat/lab/usercontacts/controller/UserController.java
+++ src/main/java/pl/edu/wat/lab/usercontacts/controller/UserController.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
 
+
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
